874fae6070c0289d762f5293e73a820e5676de96
pl23 <61396518+pl23@users.noreply.github.com>
2023-10-29 16:25:24 -0400

    sup

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import getpass
+import socket
+import random
+import numpy
+from pprint import pprint
+
+
+class input_key:
+    def __init__ (self,event)
+
+
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+if __name__ == '__main__':
+    main()
